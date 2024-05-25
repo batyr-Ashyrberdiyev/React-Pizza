65a4a743f764f532664c09db3317514255cbf06a
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-05-25 19:36:14 +0500

    added category names

---
+++ src/lib/database/category.data.ts
@@ -0,0 +1,22 @@
+export const categoriesData = [
+  {
+    view: 'Все',
+    id: 'all',
+  },
+  {
+    view: 'Мясные',
+    id: 'meat',
+  },
+  {
+    view: 'Гриль',
+    id: 'grill',
+  },
+  {
+    view: 'Острые',
+    id: 'speecy',
+  },
+  {
+    view: 'Закрытые',
+    id: 'close',
+  },
+];
