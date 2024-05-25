24f13875e97142a3b0a8f1001ddfc623735d997a
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-05-25 19:36:31 +0500

    added header and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { Outlet } from 'react-router-dom';
+import { Header } from './components/Header';
+
 function App() {
-  return <div className=""></div>;
+  return (
+    <div className="bg-mainYellowBg px-[50px] py-10">
+      <div className="bg-white rounded-[10px] py-10">
+        <Header />
+        <Outlet />
+      </div>
+    </div>
+  );
 }
 
 export default App;
