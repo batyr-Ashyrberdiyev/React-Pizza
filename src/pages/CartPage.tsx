325a99ce4ffa6d3049ae77c961db3591095b68bf
batyr-Ashyrberdiyev <awbatyr@gmail.com>
2024-05-26 19:48:17 +0500

    new cart page process

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,5 +1,9 @@
 const CartPage = () => {
-  return <div>CartPage</div>;
+  return (
+    <div className="container pt-10">
+      <h1 className="h1">Корзина</h1>
+    </div>
+  );
 };
 
 export default CartPage;
