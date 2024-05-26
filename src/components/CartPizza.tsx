77088d704828d861eaae7d0792e9b85fa975dfed
batyr-Ashyrberdiyev <awbatyr@gmail.com>
2024-05-26 19:45:56 +0500

    process cart pizza

---
+++ src/components/CartPizza.tsx
@@ -0,0 +1,10 @@
+const CartPizza = () => {
+  return (
+    <div className="flex items-center justify-between">
+      <img src="" alt="pizza" />
+      <h2 className="h2"></h2>
+    </div>
+  );
+};
+
+export default CartPizza;
