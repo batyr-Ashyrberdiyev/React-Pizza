fc093466fde22b7f9c0e58ebf0eac177dacd4719
batyr-Ashyrberdiyev <awbatyr@gmail.com>
2024-05-26 19:47:47 +0500

    added new status and functions for categories state

--- src/lib/zustand/useCategory.ts
+++ src/lib/zustand/useCategory.ts
@@ -1,21 +1,21 @@
-import { create } from 'zustand';
+import { create } from "zustand";
 
-type ActiveCategoryType = {
+export type ActiveCategoryType = {
   view: string;
   id: string;
 };
 
 type CategoryStore = {
   activeCategory: ActiveCategoryType;
-
   setCategory: (obj: ActiveCategoryType) => void;
 };
 
 export const useCategory = create<CategoryStore>((set) => ({
   activeCategory: {
-    view: 'Все',
-    id: 'all',
+    view: "Все",
+    id: "all",
   },
-
-  setCategory: (obj) => set((state) => ({ activeCategory: (state.activeCategory = obj) })),
+  active: false,
+  setCategory: (obj) =>
+    set((state) => ({ activeCategory: (state.activeCategory = obj) })),
 }));
