d83920535f289032e00f1213ade9edbcea19b05d
batyr-Ashyrberdiyev <awbatyr@gmail.com>
2024-05-26 19:48:44 +0500

    added response media for pizzas

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,24 +1,26 @@
-import Category from '../components/Category';
-import { PizzaCard } from '../components/PizzaCard';
-import { SortModal } from '../components/SortModal';
-import { categoriesData } from '../lib/database/category.data';
-import useGetPizzas from '../lib/hooks/useGetPizzas';
-import { useCategory } from '../lib/zustand/useCategory';
-import { v4 } from 'uuid';
+import Category from "../components/Category";
+import { PizzaCard } from "../components/PizzaCard";
+import { SortModal } from "../components/SortModal";
+import { categoriesData } from "../lib/database/category.data";
+import useGetPizzas from "../lib/hooks/useGetPizzas";
+import { useCategory } from "../lib/zustand/useCategory";
+import { v4 } from "uuid";
 
 const HomePage = () => {
   const { pizzasData } = useGetPizzas();
 
   const activeCategory = useCategory((state) => state.activeCategory);
 
-  console.log(activeCategory.id);
-
   return (
     <div className="container pt-10">
-      <div className="flex items-center justify-between mb-20">
-        <div className="flex items-center gap-5">
+      <div className="flex lg:flex-row flex-col gap-5 lg:items-center justify-between mb-20">
+        <div className="flex  items-center gap-5">
           {categoriesData.map((item) => (
-            <Category key={v4()} active={activeCategory.id === item.id} {...item} />
+            <Category
+              key={v4()}
+              active={activeCategory.id === item.id}
+              {...item}
+            />
           ))}
         </div>
 
@@ -26,12 +28,10 @@ const HomePage = () => {
       </div>
 
       <h1 className="h1 mb-10">Все пиццы</h1>
-      <div className="grid grid-cols-4 justify-items-center gap-x-5 gap-y-10">
-        {pizzasData && activeCategory.id === 'all'
-          ? pizzasData?.map((item) => <PizzaCard key={v4()} {...item} />)
-          : pizzasData
-              ?.filter((item) => item.category)
-              .map((pizza) => <PizzaCard key={v4()} {...pizza} />)}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-items-center gap-x-5 gap-y-10">
+        {pizzasData?.map((item) => (
+          <PizzaCard key={v4()} {...item} />
+        ))}
       </div>
     </div>
   );
